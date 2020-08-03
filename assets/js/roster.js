db6524ce0dd3754e47e0d724c7b2003aad53d540
Frankie <dracko@hotmail.co.uk>
2020-08-03 16:05:18 +0000

    Shorten list to 11 and sorted out

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -1,4 +1,4 @@
-// Define the method, mode and the headers
+// Define the method, mode and the headers for the fetch JSON
 var myInit = { method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
@@ -14,47 +14,43 @@ fetch(myRequest)
     .then(function(resp) {
         return resp.json();
     })
-    .then(newFunction());
+    .then(makeTable());
+    
 
 
 
-
-
-function newFunction() {
+function makeTable() {
     return function (data) {
         var el = document.getElementById("data");
-        // el.innerHTML = "";
-        // var dataRow = [];
-        // var tableRow = [];
-        // memberArray = [];
-        // for (i = 0; i < 10; i++) {
-        //     var memberName = (data.members[i].character.name);
-        //     memberArray.push(memberName);
-        //     console.log(memberArray);
-        // };
-        // dataRow.push(`<td>${memberArray}</td>`);
-        // tableRow.push(`<tr>${dataRow}</tr>`);
-        // console.log(dataRow);
-        // console.log(tableRow);
+        el.innerHTML = "";
+        data = data.members;
 
-        var data = data.members;
+        data.sort((a, b) => {
+            var keyA = a.rank;
+            var keyB = b.rank;
+            if (keyA < keyB) return -1;
+            if (keyA > keyB) return 1;
+            return 0;
+        });
+
         var dataRow = [];
         var tableRow = [];
-        
+        console.log(data);
         data.forEach(function (item) {
-            
+
+
             Object.keys(item).forEach(function (key) {
-                obj = item[key];
+
+                 var obj = item[key];
                 if (key == "character") {
-                var rowDataName = obj.name;
-                var rowDataLevel = obj.level;
-                dataRow.push(`<tr><td>${rowDataName}</td><td>${rowDataLevel}</td></tr>`);
-                console.log(dataRow);
-                }    
+                    var rowDataName = obj.name;
+                    var rowDataLevel = obj.level;
+                    dataRow.push(`<tr><td>${rowDataName}</td><td>${rowDataLevel}</td></tr>`);
+                }
             });
-              
+
+
             el.innerHTML = `<table>${dataRow}</table>`.replace(/,/g, "");
         });
-    }
+    };
 }
-   
